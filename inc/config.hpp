d57ec3a10a0698ff00c1db8068ed91fbf6ea799c
keistad-root <asdff46@pusan.ac.kr>
2025-07-24 09:16:13 +0900

    경로 수정

--- inc/config.hpp
+++ inc/config.hpp
@@ -1,6 +1,6 @@
 #ifndef __CONFIG_H_IN__
 
-#define BINARY_DIR "/Users/ychoi/source/xRayGeant4/build"
-#define SOURCE_DIR "/Users/ychoi/source/xRayGeant4"
+#define BINARY_DIR "/Users/ychoi/source/MPSS/build"
+#define SOURCE_DIR "/Users/ychoi/source/MPSS"
 
 #endif
